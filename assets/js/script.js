16109a32eec100b9ce62391de78a758ea4c22128
FRANCESCO <OLIVOTTI>
2020-12-01 15:21:13 +0100

    inserito   loop while per generare numeri random non doppi

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 /* js-simon
-Un alert espone 5 numeri casuali diversi.
-Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
+
+
 Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
 Consigli del giorno:
 * Pensate prima in italiano.
@@ -12,16 +12,44 @@ var RandomNumbersList = [];
 var NumeriUtenteList = [];
 var numeroUtentePrompt;
 var RandomNum;
+//Un alert espone 5 numeri casuali diversi.
+while (RandomNumbersList.length < 5) {
+    //inserisco solo se il numero non è già presente nell'array
+    RandomNum = genRandomNumbers(1, 500);
+    var cerca = presenteInArray(RandomNumbersList, RandomNum);
+    if (cerca == false) {
+      RandomNumbersList.push(RandomNum);
+    }
+  }
 
-for ( var i = 0; i <5 ; i++) {
-    RandomNum = genRandomNumbers(1,500);
-    console.log(RandomNum);
-    RandomNumbersList.push(RandomNum);
-}
+
 console.log(RandomNumbersList);
 alert ("Ciao cerca di memorizzare questi cinque numeri, hai 30 secondi da adesso per farlo." + " " + RandomNumbersList)
 
 
+
+//Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
+
+
+
+
+
+
+//Funzioni generiche per l'esercizio
+
 function genRandomNumbers(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+
+  function presenteInArray(array, element) {
+    var i = 0;
+    var result = false;
+    while (i < array.length && result == false) {
+      if (array[i] == element) {
+        result = true;
+      }
+      i++;1
+    }
+    return result;
   }
